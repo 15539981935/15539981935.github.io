52a0e097d4158c4a5160e6ea3b4873bd8eb2320b
黄斌 <>
2019-02-19 16:02:32 +0800

    s

---
+++ js/index.js
@@ -0,0 +1,37 @@
+nie.define("Index",function(){
+     
+    var components={
+        fun1:function(){
+
+        },
+        fun2:function(){
+
+        },
+        fun3:function(){
+
+        },
+        fun4:function(){
+
+        },
+        fun5:function(){
+
+        },
+        fun6:function(){
+
+        },
+        fun7:function(){
+
+        },
+        init:function(){
+            this.fun1;
+            this.fun2;
+            this.fun3;
+            this.fun4;
+            this.fun5;
+            this.fun6;
+            this.fun7;
+        }
+    }
+
+});
+
