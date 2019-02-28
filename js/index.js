4e5f6f6b6aca618956972fb71e42ac754da51b0b
黄斌 <>
2019-02-28 11:02:50 +0800

    s

--- js/index.js
+++ js/index.js
@@ -1,37 +1,69 @@
-nie.define("Index",function(){
-     
-    var components={
-        fun1:function(){
+var vm = new Vue({
+    el: "#app",
+    data() {
+        return {
+            userid: "",
+            username: "",
+            userage: "",
 
-        },
-        fun2:function(){
-
-        },
-        fun3:function(){
-
-        },
-        fun4:function(){
-
-        },
-        fun5:function(){
-
-        },
-        fun6:function(){
-
-        },
-        fun7:function(){
-
-        },
-        init:function(){
-            this.fun1;
-            this.fun2;
-            this.fun3;
-            this.fun4;
-            this.fun5;
-            this.fun6;
-            this.fun7;
+            userlist: [{
+                    id: '1',
+                    name: "周星驰",
+                    age: "55",
+                    time: new Date()
+                },
+                {
+                    id: '2',
+                    name: "成龙",
+                    age: "62",
+                    time: new Date()
+                },
+                {
+                    id: '3',
+                    name: "李连杰",
+                    age: "65",
+                    time: new Date()
+                },
+                {
+                    id: '4',
+                    name: "吴京",
+                    age: "36",
+                    time: new Date()
+                }
+            ]
         }
-    }
+    },
+    methods: {
+        add() {
+            if (this.userid && this.username && this.userage) {
+                this.userlist.push({
+                    id: this.userid,
+                    name: this.username,
+                    age: this.userage,
+                    time: new Date()
+                })
+                this.userid = this.username = this.userage = null;
+            } else {
+                alert("请填写完整！")
+            }
+        },
+        del(listId) {
+            // this.userlist.some((item, i) => {
+            //     if (item.id === listId) {
+            //         this.userlist.splice(i, 1);
+            //         return true;
+            //     }
+            // })
 
-});
+            var index = this.userlist.findIndex(item => {
+                if (item.id === listId) {
+                    return true;
+                }
+            })
+            this.userlist.splice(index, 1);
+        }
+    },
+    mounted() {
 
+    },
+})
